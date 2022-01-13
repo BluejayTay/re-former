ae10659dffd1eb4b8f6221abdc661def85d3749d
BluejayTay <tayrusthoven@gmail.com>
2022-01-13 15:28:26 -0600

    Add User model, controller, routes, valdidations, edit and new views

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 4..20 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8}
+  
+end
